36f0048d62444fc5b4ba27fb91eddcd481d9bac7
lizdo <lizdourden@gmail.com>
2011-07-03 21:25:43 +0800

    Some missing resources.

---
+++ ConvertAudioForiPhone.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+### Convert Audio for iPhone Project
+import os.path
+import subprocess
+import shutil
+
+def main():
+	dir = '/Users/Liz/Dropbox/Projects/BoardGame/Sound'
+	files = os.listdir(dir)
+	os.chdir(dir)
+	for file in files:
+		if file.endswith('m4a'):
+			newfile = file.replace('m4a','caf')
+			print(newfile)
+			commandlist = ['/usr/bin/afconvert', '-f', 'caff', '-d', 'LEI16',file,newfile]
+			subprocess.call(commandlist)
+			os.remove(file)
+	
+if __name__ == '__main__':
+	main()
